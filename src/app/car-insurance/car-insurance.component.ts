5560c67a4389a5abf4d269bf391fae1aa9461eaa
Prashant <131854641+prashant-d23@users.noreply.github.com>
2023-07-05 18:15:07 +0530

    progress-bar

--- src/app/car-insurance/car-insurance.component.ts
+++ src/app/car-insurance/car-insurance.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { InsuranceService } from '../core/services/insurance.service';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-car-insurance',
   templateUrl: './car-insurance.component.html',
   styleUrls: ['./car-insurance.component.scss']
 })
-export class CarInsuranceComponent {
+export class CarInsuranceComponent implements OnInit {
+  progress = 25;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.updateProgress();
+      }
+    });
+  }
+
+  updateProgress() {
+    const currentRoute = this.router.routerState.snapshot.url;
+    const routes = [
+      '/car-insurance',
+      '/car-insurance/choose-plan',
+      '/car-insurance/personal-details',
+      '/car-insurance/payment'
+    ];
+    this.progress = (routes.indexOf(currentRoute) / (routes.length - 1)) * 100;
+  }
 }
